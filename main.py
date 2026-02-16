4668664dfc36ef8584bfd72f86e1f4cf5944b798
Josh Gillette <joshtgillette@gmail.com>
2026-02-16 08:48:57 -0600

    Squashed commit of the following:
    commit 2ec764932c035908f51774723f2a86646690f4a6
    Author: Josh Gillette <joshtgillette@gmail.com>
    Date:   Mon Feb 16 08:48:23 2026 -0600
    
        Initialize student transformer for calculator distillation
    
    commit 5177e3dcd9f7b4b940ef76cbb11c91949c056635
    Author: Josh Gillette <joshtgillette@gmail.com>
    Date:   Mon Feb 16 08:45:56 2026 -0600
    
        Add cmd argument directory for models

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import sys
+
+from cali import cali
 from mlp import mlp
+
+PROJECTS = {
+    "mlp": mlp.go,
+    "cali": cali.go,
+}
 
 
 def main():
-    mlp.go()
+    if len(sys.argv) != 2 or sys.argv[1] not in PROJECTS:
+        print(f"Usage: python main.py <{'|'.join(PROJECTS.keys())}>")
+        sys.exit(1)
+
+    PROJECTS[sys.argv[1]]()
 
 
 if __name__ == "__main__":
